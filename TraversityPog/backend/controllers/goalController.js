22e6e88db8803bac82182d097dcbca2639a9389e
Hachikoi-the-creator <facka72@gmail.com>
2022-03-22 19:45:55 -0600

    GET, POST, PUT, DEL methods workinggit add .

--- TraversityPog/backend/controllers/goalController.js
+++ TraversityPog/backend/controllers/goalController.js
@@ -1,39 +1,64 @@
 const asyncHandle = require('express-async-handler');
+const { default: mongoose } = require('mongoose');
+const Goal = require('../models/goalModel');
+
 //1 @Desc    Get Goal
 //1 @Route   GET api/goals
 //1 @Acces   Private
 const getGoals = asyncHandle(async (req, res) => {
-  res.status(200).json({ msg: "Get Goals" });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 //2 @Desc    Set Goal
 //2 @Route   POST api/goals
 //2 @Acces   Private
 const setGoal = asyncHandle(async (req, res) => {
-  // TODO: FIX THIS  LATER LOL (hard-coded prop[num])
-  if (!req.body.number) {
+  if (!req.body.text) {
     res.status(400);
     throw new Error(`Bro don't be like this plese give me data`);
   }
+  const goal = await Goal.create({
+    text: req.body.text
+  });
 
-  res.status(200).json({ msg: `Set Goal` });
+  res.status(200).json(goal);
 });
 
 //3 @Desc    Update Goal
 //3 @Route   PUT api/goals/:id
 //3 @Acces   Private
 const updateGoal = asyncHandle(async (req, res) => {
-  console.log(req.body);
-  // const idy = req.params.id;
-  res.status(200).json({ msg: `Update Goal ${req.params.id}` });
+  const idy = req.params.id;
+  const goal = await Goal.findById(idy);
+  // const anotherIf = await Goal.findById(req.params.id)
+
+  if (!goal) {
+    res.status(400);
+    throw new Error('There was no id to find the Goal T.T');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(idy, req.body, { new: true });
+
+  res.status(200).json(updatedGoal);
 });
 
 //4 @Desc    Delete Goal
 //4 @Route   DELETE api/goals/:id
 //4 @Acces   Private
 const deleteGoal = asyncHandle(async (req, res) => {
-  // const idy = req.params.id;
-  res.status(200).json({ msg: `Delete Goal ${req.params.id}` });
+  const idy = req.params.id;
+  const goal = await Goal.findById(idy);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error('There was no id to find the Goal T.T - cannot DELETE');
+  }
+
+  const deletedGoal = await Goal.findByIdAndDelete(idy);
+
+  res.status(200).json(deletedGoal);
 });
 
 module.exports = {
